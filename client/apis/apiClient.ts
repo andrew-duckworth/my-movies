c6d22a202606a1bd03edf8a6f0d5d50575067ce7
Estrella <122237413+estrella-mooney@users.noreply.github.com>
2023-03-29 11:34:54 +1300

    Create apiClient file, Add func Hikes

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -1,1 +1,6 @@
-import r
+import request from 'superagent'
+import { HikesInterface } from '../../common/Hikes'
+
+export function getAllHikes(): Promise<HikesInterface[]> {
+  return request.get('/api/v1/hikes').then((i) => i.body)
+}
