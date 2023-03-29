57ffede442631f5273b06a34fa0e733c771f3308
Olivia Clarke-Edwards <short.livi@gmail.com>
2023-03-30 11:54:52 +1300

    add client side apis to access db routes

--- client/apis/jokeApi.ts
+++ client/apis/jokeApi.ts
@@ -1,8 +1,25 @@
 import request from 'superagent'
-import { Joke } from '../../common/models'
+import { Joke, UserJoke } from '../../common/models'
 
 const jokesURL = '/api/v1/database/jokes'
 
 export function fetchJokes(): Promise<Joke[]> {
   return request.get(`${jokesURL}/all`).then((jokes) => jokes.body)
+}
+
+export function fetchJokeById(id: number): Promise<Joke> {
+  return request.get(`${jokesURL}/${id}`).then((joke) => joke.body)
+}
+
+export function sendNewJoke(joke: UserJoke): Promise<Joke> {
+  return request
+    .post(`${jokesURL}/add`)
+    .send(joke)
+    .then((joke) => joke.body)
+}
+
+export function removeJokeById(id: number): Promise<number> {
+  return request
+    .delete(`${jokesURL}/delete/${id}`)
+    .then((numberDeleted) => numberDeleted.body)
 }
