8bace2e15a134d048997987f31091ea80f86f007
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-03-29 20:25:41 +1300

    planning

--- client/apis/booksApi.ts
+++ client/apis/booksApi.ts
@@ -4,3 +4,12 @@ import { Book } from '../../common/interfaces'
 export function fetchAllBooks(): Promise<Book[]> {
   return request.get('/api/v1/books').then((bookres) => bookres.body)
 }
+
+/*
+export function insertWidget(widgetData: WidgetData): Promise<Widget> {
+  return request
+    .post(widgetUrl)
+    .send(widgetData)
+    .then((res) => res.body)
+}
+*/
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -25,6 +25,7 @@ export default App
 // https://openlibrary.org/developers/api
 // https://openlibrary.org/dev/docs/api/covers
 // https://api.rawg.io/docs/
+
 // References: https://github.com/kahu-2023/consuming-external-apis/tree/ryan https://github.com/kahu-2023/code-from-class/tree/main/w6d3-scss
 // title, publish_date, author_name
 // Will need to turn multiple authors into comma separated list
@@ -32,4 +33,5 @@ export default App
 // Covers are hard - sometimes the API only has data for particular editions
 // Publish date formats are inconsistent
 
-// Finish book seeds
+// booksapi.ts add and remove book
+// add and remove book components -> process raw book data in api 
