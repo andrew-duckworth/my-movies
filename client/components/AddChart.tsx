c138e84daa1610772efa0c7c11d4d168f288a352
Shadae <shadaejadeellmers@gmail.com>
2023-03-30 14:40:01 +1300

    WIP i lied css done for now

--- client/components/AddChart.tsx
+++ client/components/AddChart.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, FormEvent, useState } from 'react'
 
 export function AddChart() {
   const [data, setData] = useState({
+    name: '',
     sun: '',
     moon: '',
     rising: '',
@@ -23,30 +24,42 @@ export function AddChart() {
     <div className="add">
       <h2>Enter your big three: </h2>
       <form onSubmit={submitHandler}>
-        <label htmlFor="sunSign">Sun Sign: </label>
-        <input
-          type="text"
-          value={data.sun}
-          name="sun"
-          onChange={changeHandler}
-        />
-        <br />
-        <label htmlFor="moonSign">Moon Sign: </label>
-        <input
-          type="text"
-          value={data.moon}
-          name="moon"
-          onChange={changeHandler}
-        />
-        <br />
-        <label htmlFor="risingSign">Rising Sign: </label>
-        <input
-          type="text"
-          value={data.rising}
-          name="rising"
-          onChange={changeHandler}
-        />
-        <br />
+        <div className="text-item">
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            value={data.name}
+            name="name"
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="text-item">
+          <label htmlFor="sunSign">Sun Sign: </label>
+          <input
+            type="text"
+            value={data.sun}
+            name="sun"
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="text-item">
+          <label htmlFor="moonSign">Moon Sign: </label>
+          <input
+            type="text"
+            value={data.moon}
+            name="moon"
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="text-item">
+          <label htmlFor="risingSign">Rising Sign: </label>
+          <input
+            type="text"
+            value={data.rising}
+            name="rising"
+            onChange={changeHandler}
+          />
+        </div>
         <button>Submit</button>
       </form>
     </div>
