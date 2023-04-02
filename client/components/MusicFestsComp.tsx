179c4816f70ead5592ea44e20c007e738327f681
Josh Mason <josh.mason.music@gmail.com>
2023-04-02 17:06:17 +1200

    connect up front and back ends

--- client/components/MusicFestsComp.tsx
+++ client/components/MusicFestsComp.tsx
@@ -1,23 +1,38 @@
 import { useAppDispatch, useAppSelector } from '../hooks/redux'
+import { fetchMusicFests } from '../actions/musicFestival'
+import { useEffect, useState } from 'react'
+import { Link, Routes, Route } from 'react-router-dom'
+import type { MusicFestivalDetails } from '../../common/musicFestivalsTypes'
 
 function MusicFestivals() {
-  const musicFestivals = useAppSelector(
-    (globalState) => globalState.musicFestRdcrs
-  )
+  const dispatch = useAppDispatch()
 
-  console.log('In components >>>' + musicFestivals)
+  const festsFromStore = useAppSelector((state) => state.musicFestRdcrs)
+  const firstFestName = festsFromStore[0].name
+  console.log(festsFromStore)
+  useEffect(() => {
+    dispatch(fetchMusicFests())
+  }, [dispatch])
+
   return (
-    <div>
-      <h1>Here is where the music fests be at:</h1>
-      <p>temp hardcoded data</p>
-      <p>Whoa, we&apos;re going to Ibiza</p>
-      <p>Rampage Belgium</p>
-      <ul>
-        {musicFestivals.map((fest) => (
-          <li key={fest.name}>{fest.name}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>
+        <h1>Here is where the music fests be at:</h1>
+        <p>temp hardcoded data</p>
+        <p>Whoa, we&apos;re going to Ibiza</p>
+
+        {festsFromStore.map((singleFest: MusicFestivalDetails) => {
+          return (
+            <div key={singleFest.id}>
+              <h2>{singleFest.name}</h2>
+
+              <p>{singleFest.country}</p>
+              <p>{singleFest.date}</p>
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
