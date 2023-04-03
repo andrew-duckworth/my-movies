746653cf793f75ca0521a35800e9ac4c05ddfe02
Josh Liuaana <jfliuaana@gmail.com>
2023-04-04 00:19:57 +1200

    update seeds because me love love

--- server/db/seeds/movies.js
+++ server/db/seeds/movies.js
@@ -16,5 +16,19 @@ exports.seed = async function (knex) {
         'https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.6757_AL_.jpg',
       rating: 4,
     },
+    {
+      id: 3,
+      title: 'Titanic',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_Ratio0.6757_AL_.jpg',
+      rating: 13,
+    },
+    {
+      id: 4,
+      title: 'The Vow',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BMjE1OTU5MjU0N15BMl5BanBnXkFtZTcwMzI3OTU5Ng@@._V1_Ratio0.6757_AL_.jpg',
+      rating: 9,
+    },
   ])
 }
