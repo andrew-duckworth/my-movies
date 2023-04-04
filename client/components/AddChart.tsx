62596fd41c2be559a88f96207f3d413d2ba577c6
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-04-05 08:44:17 +1200

    WIP component for form to add

---
+++ client/components/AddChart.tsx
@@ -0,0 +1,48 @@
+function AddChart() {
+  return (
+    <section>
+      <div className="add">
+        <h2>Enter your big three: </h2>
+        {/* <form onSubmit={submitHandler}>
+          <div>
+          <label htmlFor="name">newName: </label>
+          <input
+            type="text"
+            value={data.newName}
+            name="newName"
+            onChange={changeHandler}
+          />
+          </div>
+          <div className="text-item">
+          <label htmlFor="newSun">Sun Sign: </label>
+          <input
+            type="text"
+            value={data.newSun}
+            name="newSun"
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="text-item">
+          <label htmlFor="newMoon">Moon Sign: </label>
+          <input
+            type="text"
+            value={data.newMoon}
+            name="newMoon"
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="text-item">
+          <label htmlFor="newRising">Rising Sign: </label>
+          <input
+            type="text"
+            value={data.newRising}
+            name="rnewRising"
+            onChange={changeHandler}
+          />
+        </form> */}
+      </div>
+    </section>
+  )
+}
+
+export default AddChart
