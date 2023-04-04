b08015bcf9613d203faf54a025e3b9401974d892
Josh Liuaana <jfliuaana@gmail.com>
2023-04-05 10:52:18 +1200

    delete server log

--- server/index.ts
+++ server/index.ts
@@ -11,5 +11,4 @@ const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port', PORT)
-  console.log(process.env.IMDB_KEY);
 })
