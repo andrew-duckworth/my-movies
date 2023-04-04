a2db5a5dfcc4767a696731d4bc7ff591585db211
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-04-04 17:50:11 +1200

    route set up for add user

--- server/routes/bigthree.ts
+++ server/routes/bigthree.ts
@@ -1,16 +1,25 @@
 import express from 'express'
-import { getUsers } from '../db/db'
+import * as db from '../db/db'
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  getUsers()
+  db.getUsers()
     .then((users) => {
       res.json(users)
     })
     .catch((err) => {
       res.status(500).send(err.message)
     })
+})
+
+router.post('/', (req, res) => {
+  const user = req.body
+  db.addUser(user)
+    .then((userArr) => {
+      return res.json(userArr[0])
+    })
+    .catch((err) => console.log(err.essage))
 })
 
 export default router
