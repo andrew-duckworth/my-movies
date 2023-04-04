7156f41392a5cc160f1a4afbb5c272d755f85658
Shadae <shadaejadeellmers@gmail.com>
2023-04-04 19:57:04 +1200

    api set up for add user

--- client/apis/starsigns.ts
+++ client/apis/starsigns.ts
@@ -8,3 +8,11 @@ export function grabSigns() {
 export function grabUsers() {
   return request.get('/api/v1/bigthree').then((res) => res.body)
 }
+
+export function addAUser(user: BigThree): Promise<BigThree> {
+  return request
+    .post('/api/v1/bigthree')
+    .send(user)
+    .then((res) => res.body)
+    .catch((err) => console.log(err.message))
+}
