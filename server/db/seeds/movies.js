e9dcc89886473f8049334dfd341a3d4c3fcaeb8c
Josh Liuaana <jfliuaana@gmail.com>
2023-04-04 13:54:39 +1200

    update migrations seeds

--- server/db/seeds/movies.js
+++ server/db/seeds/movies.js
@@ -4,27 +4,15 @@ exports.seed = async function (knex) {
   await knex('movies').insert([
     {
       id: 1,
-      title: 'Inception',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_Ratio0.6800_AL_.jpg',
-      rating: 8,
-    },
-    {
-      id: 2,
-      title: 'The Batman',
-      image:
-        'https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.6757_AL_.jpg',
-      rating: 4,
-    },
-    {
-      id: 3,
+      imdb_id: 'tt0120338',
       title: 'Titanic',
       image:
         'https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_Ratio0.6757_AL_.jpg',
       rating: 13,
     },
     {
-      id: 4,
+      id: 2,
+      imdb_id: 'tt1606389',
       title: 'The Vow',
       image:
         'https://m.media-amazon.com/images/M/MV5BMjE1OTU5MjU0N15BMl5BanBnXkFtZTcwMzI3OTU5Ng@@._V1_Ratio0.6757_AL_.jpg',
