68758325518fae2a6af36925f6285b63ecfbb647
Gerard Verhaart <121985303+gerard-verhaart@users.noreply.github.com>
2023-04-05 17:30:07 +1200

    WIP decorator to throttle asynch

--- client/apis/scryfall.ts
+++ client/apis/scryfall.ts
@@ -1,5 +1,7 @@
 import request from 'superagent'
+import { throttle } from 'lodash';
 
+@throttle_async(100)
 export function getCompletions(name: string): Promise<string[]> {
   return request
     .get(
@@ -10,6 +12,7 @@ export function getCompletions(name: string): Promise<string[]> {
     .then((req) => req.body.data)
 }
 
+@throttle_async(100)
 export function getCardArt(
   name: string
 ): Promise<{ image: string; img_attribution: string }> {
@@ -25,4 +28,14 @@ export function getCardArt(
         },
       }) => ({ image: art_crop, img_attribution: artist })
     )
+}
+
+function throttle_async<Args extends any[], Return>(wait: number) {
+  return (func: (...args: Args) => Return) => {
+    const throttled = throttle(func, wait)
+    return (...args: Args) => {
+      const ret = throttled(...args)
+      return ret !== undefined ? ret : Promise.reject(Error('Throttled promise returned undefined'))
+    }
+  }
 }
