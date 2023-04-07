ae1ce4a7731f68ea22d56860d8ab5a4e26f8a22f
humaidiar <humaidiar123@gmail.com>
2023-04-08 09:40:47 +1200

    fixing the sticky form nav

--- client/components/CoffeesStyle.tsx
+++ client/components/CoffeesStyle.tsx
@@ -1,41 +1,37 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks/redux'
 import { fetchSetCoffee } from '../actions/getCoffee'
 import SingleCoffee from './CoffeeSingle'
 import AddMethodForm from './FormCoffee'
 import LoadingScreen from './LoadingScreen'
 import FooterBar from './Footer'
-import UpdateForm from './FormUpdate'
 
 export default function AllCoffee() {
   const dispatch = useAppDispatch()
   const coffees = useAppSelector((state) => state.coffeeReducer)
-  const [toggle, setToggle] = useState(false)
-
-  const handleClose = () => {
-    setToggle(false)
-  }
 
   useEffect(() => {
     dispatch(fetchSetCoffee())
   }, [dispatch])
 
   return (
-    <section className="container">
-      <div className="form-wrapper">
-        <AddMethodForm />
+    <section>
+      <div className="container">
+        <div className="form-wrapper">
+          <AddMethodForm />
+        </div>
+        <div className="card-list">
+          {coffees
+            .slice()
+            .reverse()
+            .map((coffee) => (
+              <div key={coffee.id}>
+                <SingleCoffee coffeeProp={coffee} />
+              </div>
+            ))}
+        </div>
       </div>
-      <div className="card-list">
-        {coffees
-          .slice()
-          .reverse()
-          .map((coffee) => (
-            <div key={coffee.id}>
-              <SingleCoffee coffeeProp={coffee} />
-            </div>
-          ))}
-        <FooterBar />
-      </div>
+      <FooterBar />
       <LoadingScreen />
     </section>
   )
