370bb1b8d0db50e0e4ff710fba3b24d6e607f4c6
Louis Dyer <louisalexanderdyer@gmail.com>
2023-04-10 20:29:05 +1200

    Add types

--- client/apis/imdb.ts
+++ client/apis/imdb.ts
@@ -3,7 +3,7 @@ import { Imdb } from '../../common/types'
 
 const url = 'https://imdb-api.com/en/API/SearchMovie'
 
-export function searchMoviesAPI(movie: string) {
+export function searchMoviesAPI(movie: string): Promise<Imdb[]> {
   return request
     .get(`${url}/${process.env.IMDB_KEY}/${movie}`)
     .then((res) => res.body.results)
