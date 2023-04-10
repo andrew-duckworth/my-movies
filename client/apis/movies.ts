d3f278f55dadbd15d3ac81ff315570c5fe5af7f4
Louis Dyer <louisalexanderdyer@gmail.com>
2023-04-10 17:17:31 +1200

    Going home for the day, partial commit, was working on external API stuff

--- client/apis/movies.ts
+++ client/apis/movies.ts
@@ -6,3 +6,7 @@ const movieURL = '/api/v1/movies'
 export function fetchMoviesAPI(): Promise<Movie[]> {
   return request.get(movieURL).then((res) => res.body)
 }
+
+// https://imdb-api.com/en/API/SearchMovie/${process.env.IMDB_KEY}/inception%202010
+// https://imdb-api.com/en/API/SearchMovie/${process.env.IMDB_KEY}/inception 2010 - corrects to above
+// https://imdb-api.com/en/API/SearchMovie/${process.env.IMDB_KEY}/inception
