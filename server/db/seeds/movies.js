ce78da6467eb6d67ce43315da36ae33101ccd368
Louis Dyer <louisalexanderdyer@gmail.com>
2023-04-10 17:03:57 +1200

    setting up IMDB .env, API key & dotenv

--- server/db/seeds/movies.js
+++ server/db/seeds/movies.js
@@ -8,18 +8,21 @@ exports.seed = async function (knex) {
   await knex('movies').insert([
     {
       id: 1,
+      imdb_id: 'tt0120338',
       title: 'Inception',
       image: 'https://picsum.photos/500/750',
       rating: 5,
     },
     {
       id: 2,
+      imdb_id: 'tt0120338',
       title: 'Home Alone',
       image: 'https://picsum.photos/500/700',
       rating: 5,
     },
     {
       id: 3,
+      imdb_id: 'tt0120338',
       title: 'Bat Man',
       image: 'https://picsum.photos/500/725',
       rating: 5,
