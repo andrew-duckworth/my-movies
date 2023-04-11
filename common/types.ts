f8a1742baac01dd6dcef99e3dfbf69a331b2b456
Louis Dyer <louisalexanderdyer@gmail.com>
2023-04-11 17:28:42 +1200

    Finished add route with a lil help from GPT

--- common/types.ts
+++ common/types.ts
@@ -9,6 +9,8 @@ export interface Data {
 
 export interface Movie extends Data {
   id: number
+  [Symbol.iterator](): Iterator<Movie>
+  //Got this code from ChatGPT and I don't understand it but it fixes my TS issues in server/routes/movies
 }
 
 export interface Imdb {
