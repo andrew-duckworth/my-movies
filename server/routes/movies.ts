15baf7b1f02d98e666127b2efc0bf4be92d50451
Louis Dyer <louisalexanderdyer@gmail.com>
2023-04-11 17:22:19 +1200

    Fix typos, small TS error to fix in server/routes/movies

--- server/routes/movies.ts
+++ server/routes/movies.ts
@@ -2,11 +2,21 @@ import express from 'express'
 const router = express.Router()
 
 import * as db from '../db/db'
-//import { getAllMoviesDB } from '../db/db'
 
 router.get('/', (req, res) => {
   db.getAllMoviesDB()
     .then((moviesArr) => res.json(moviesArr))
+    .catch((err) => {
+      res.send(500).json({ message: err.message })
+    })
+})
+
+router.post('/', (req, res) => {
+  const movie = req.body
+  db.addMovieDB(movie)
+    .then(([movieArr]) => {
+      res.json(movieArr)
+    })
     .catch((err) => {
       res.send(500).json({ message: err.message })
     })
