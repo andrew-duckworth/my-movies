6e51511e44cfe989ffd2d16c6a070b78bb455519
humaidiar <humaidiar123@gmail.com>
2023-04-11 15:34:54 +1200

    Setting up for image uploader

---
+++ server/routes/uploader.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express'
+import { UploadedFile } from 'express-fileupload'
+import { readdir, writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+  const files = await (
+    await readdir(join(__dirname, '../public/images'))
+  ).reverse()
+  res.json(files)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const image = req.files?.image as UploadedFile
+    const fileType = image.mimetype.split('/')[1]
+    const fileData = image.data
+    const name = `${Date.now()}.${fileType}`
+
+    await writeFile(join(__dirname, '../public/images', name), fileData)
+
+    res.json({ name })
+  } catch (e) {
+    res.status(500).json({ msg: (e as Error).message })
+  }
+})
+export default router
