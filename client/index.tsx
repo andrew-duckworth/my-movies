8dd9970cf575a731fb6c14f35d2340ba50cc9e01
Andrew Duckworth <a.duckworth96@live.com>
2023-04-24 19:00:25 +1200

    log in working

--- client/index.tsx
+++ client/index.tsx
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <Auth0Provider
       domain="dev-twiee8khztsocz05.au.auth0.com"
-      clientId="I0s1M23YHAxW9esrjXj52HDoia5R0ffL"
-      redirectUri={window.location.origin as string}
+      clientId="UcVo3qo54eyOHTyicUXAJ2s843JZ5dR9"
+      redirectUri={window.location.origin}
       audience="https://movies/api"
     >
       <Router>
